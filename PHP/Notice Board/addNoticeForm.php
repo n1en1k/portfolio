de255184bea63659cb87e0189f1f464267aa18b2
n1en1k <niko.nieminen93@gmail.com>
2021-03-08 18:43:21 +0200

    Style changes

--- PHP/Notice Board/addNoticeForm.php
+++ PHP/Notice Board/addNoticeForm.php
@@ -6,17 +6,17 @@
 	<body>
 		<div class="loginScreen">
 			<h2>Notice Board</h2> 
-			<h2>Add a Notice</h2> 
+			<h3>Add a Notice</h3> 
 
 			<form name="addNoticeForm" action="addNotice.php" method="post">
-			Title:<br />
-			<input type="text" maxlength="80" name="title" id="title" /><br />
-			Notice:<br />
+			<p><label for="title">Title:</label></p>
+			<input type="text" maxlength="80" name="title" id="title" />
+			<p><label for="notice">Notice:</label></p>
 			<textarea id="notice" name="notice" maxlength="1500">
-			</textarea><br />
-			Writer:<br />
-			<input type="text" name="writer" id="writer" /><br />
-			<input class="submitti" type="submit" value="Save" name="Save" />
+			</textarea>
+			<p><label for="writer">Writer:</label></p>
+			<input type="text" name="writer" id="writer" />
+			<input class="submitti" type="submit" id="save" value="Save" name="Save" />
 			</form>
 
 			<p>Important message! Only Page Admin can delete saved notices.</p>
