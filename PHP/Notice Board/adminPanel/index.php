6c063e1986be940f96501db9513bb5a83cc0f6be
n1en1k <niko.nieminen93@gmail.com>
2021-03-08 16:22:35 +0200

    Added Admin Panel index.php and .htaccess

---
+++ PHP/Notice Board/adminPanel/index.php
@@ -0,0 +1,3 @@
+<h2>Admin Page</h2>
+<p><a style="color: #ff0000;" href="deleteForm.php">Delete Notice</a></p> <!-- red color -->
+<p><a style="color: #fff600;" href="alterSelect.php">Alter Notice</a></p> <!-- yellow color -->
