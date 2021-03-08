ff1ea3ac1a35dcc67ae5dcf055d6f7fc382aa896
n1en1k <niko.nieminen93@gmail.com>
2021-03-08 17:27:21 +0200

    Minor changes

--- PHP/Notice Board/addNoticeForm.php
+++ PHP/Notice Board/addNoticeForm.php
@@ -1,14 +1,21 @@
-<h2>Add a Notice</h2> 
+<html>
+	<head>
+		<title>Add a Notice</title>
+	</head>
+	<body>
+		<h2>Add a Notice</h2> 
 
-<form name="addNoticeForm" action="addNotice.php" method="post">
-Title:<br />
-<input type="text" maxlength="80" name="title" id="title" /><br />
-Notice:<br />
-<textarea id="notice" name="notice" maxlength="1500">
-</textarea><br />
-Writer:<br />
-<input type="text" name="writer" id="writer" /><br />
-<input class="submitti" type="submit" value="Save" name="Save" />
-</form>
+		<form name="addNoticeForm" action="addNotice.php" method="post">
+		Title:<br />
+		<input type="text" maxlength="80" name="title" id="title" /><br />
+		Notice:<br />
+		<textarea id="notice" name="notice" maxlength="1500">
+		</textarea><br />
+		Writer:<br />
+		<input type="text" name="writer" id="writer" /><br />
+		<input class="submitti" type="submit" value="Save" name="Save" />
+		</form>
 
-<p>Important message! Only Page Admin can delete saved notices.</p>
+		<p>Important message! Only Page Admin can delete saved notices.</p>
+	</body>
+</html>
--- PHP/Notice Board/adminPanel/index.php
+++ PHP/Notice Board/adminPanel/index.php
@@ -1,3 +1,10 @@
-<h2>Admin Page</h2>
-<p><a style="color: #ff0000;" href="deleteForm.php">Delete Notice</a></p> <!-- red color -->
-<p><a style="color: #fff600;" href="alterSelect.php">Alter Notice</a></p> <!-- yellow color -->
+<html>
+	<head>
+		<title>Add a Notice</title>
+	</head>
+	<body>
+		<h2>Admin Page</h2>
+			<p><a style="color: #ff0000;" href="deleteForm.php">Delete Notice</a></p> <!-- red color -->
+			<p><a style="color: #fff600;" href="alterSelect.php">Alter Notice</a></p> <!-- yellow color -->
+	</body>
+</html>
