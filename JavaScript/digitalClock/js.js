b910d86b53f7680c836124d85af95a9490ecb55b
n1en1k <niko.nieminen93@gmail.com>
2022-09-04 13:04:44 +0300

    added JS digitalClock

---
+++ JavaScript/digitalClock/js.js
@@ -0,0 +1,16 @@
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("clockDisplay").innerText = time;
+    document.getElementById("clockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+}
